724d99c761b8940297e0852bfbeab55c9bb0ce88
DubDOR <77110401+DubDor@users.noreply.github.com>
2023-09-10 23:56:02 +0800

    test commit

---
+++ connect_db.php
@@ -0,0 +1,30 @@
+<?php
+// Step 1: Database Setup (usually done once)
+$servername = "your_database_server";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database_name";
+
+// Create a connection to the database
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Step 2: Connecting Your Website to the Database
+$sql = "SELECT * FROM your_table_name";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Output data from each row
+    while($row = $result->fetch_assoc()) {
+        echo "Name: " . $row["name"]. " - Email: " . $row["email"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
---
+++ script.js
@@ -0,0 +1,25 @@
+function updateDateTime() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+  const seconds = now.getSeconds().toString().padStart(2, '0');
+  const dateString = now.toLocaleDateString();
+  
+  let amPm = "AM";
+  let formattedHours = hours;
+  if (hours >= 12) {
+    amPm = "PM";
+    formattedHours = hours % 12;
+    if (formattedHours === 0) {
+      formattedHours = 12;
+    }
+  }
+
+  const timeString = `${formattedHours}:${minutes}:${seconds} ${amPm}`;
+
+  document.getElementById('time').textContent = timeString;
+  document.getElementById('date').textContent = dateString;
+}
+
+updateDateTime();
+setInterval(updateDateTime, 1000);
